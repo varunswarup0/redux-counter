9f1c2280d804e4071b6412bbb6909392c99104e7
Varun Swarup <varunswarup0@gmail.com>
2020-03-29 13:17:48 +0530

    Create Redux Store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
 import './styles.scss';
+
+const initialState = {
+  count: 0,
+};
+
+const INCREMENT = 'INCREMENT';
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      count: state.count + 1,
+    };
+  }
+  return state;
+};
+
+const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
